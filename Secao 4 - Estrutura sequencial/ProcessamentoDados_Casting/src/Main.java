ef310cdf7a8dc90dc14abb443a4605c44a544089
Pablo Ribeiro <pablorsantana@hotmail.com>
2022-12-17 18:49:59 -0300

    adicionando projeto 'Processamento de dados e casting' a secao 4

---
+++ Secao 4 - Estrutura sequencial/ProcessamentoDados_Casting/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        double c, resultado;
+
+        a = 5;
+        b = 2;
+        c = 10.5;
+
+        resultado = (double) a / b;
+
+        a = (int) c;
+
+
+        System.out.println(resultado);
+        System.out.println(a);
+
+
+    }
+}
