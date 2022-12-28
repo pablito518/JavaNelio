82f5f1809bb12e6720f9a0a1fcd1692b26b42c73
Pablo Ribeiro <pablorsantana@hotmail.com>
2022-12-28 00:08:31 -0300

    removendo objeto da classe principal e usando metodos invocando a classe diretamente

--- Secao 8 - Introducao a Programacao Orientada a Objetos/MembrosEstaticos/src/application/Program.java
+++ Secao 8 - Introducao a Programacao Orientada a Objetos/MembrosEstaticos/src/application/Program.java
@@ -8,19 +8,18 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = Calculator.circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f\n", c);
         System.out.printf("Volume: %.2f\n", v);
-        System.out.printf("PI value: %.2f\n", calc.PI);
+        System.out.printf("PI value: %.2f\n", Calculator.PI);
 
         sc.close();
     }
