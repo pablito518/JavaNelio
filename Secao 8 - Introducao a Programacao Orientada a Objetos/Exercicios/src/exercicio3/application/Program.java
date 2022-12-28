13b37e7b7699978433229ebdbee44072a96d56d8
Pablo Ribeiro <pablorsantana@hotmail.com>
2022-12-27 23:55:10 -0300

    exercicio 'Student' resolvido

--- Secao 8 - Introducao a Programacao Orientada a Objetos/Exercicios/src/exercicio3/application/Program.java
+++ Secao 8 - Introducao a Programacao Orientada a Objetos/Exercicios/src/exercicio3/application/Program.java
@@ -1,2 +1,21 @@
-package exercicio3.application;public class Program {
+package exercicio3.application;
+
+import exercicio3.entities.Student;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        student.nome = sc.nextLine();
+
+        student.primeiroTrimestre = sc.nextDouble();
+        student.segundoTrimestre = sc.nextDouble();
+        student.terceiroTrimestre = sc.nextDouble();
+
+        System.out.println(student);
+    }
 }
--- Secao 8 - Introducao a Programacao Orientada a Objetos/Exercicios/src/exercicio3/entities/Student.java
+++ Secao 8 - Introducao a Programacao Orientada a Objetos/Exercicios/src/exercicio3/entities/Student.java
@@ -1,8 +1,27 @@
 package exercicio3.entities;
 
-public class Aluno {
+import java.text.MessageFormat;
+
+public class Student {
     public String nome;
-    public double nota1;
-    public double nota2;
-    public double nota3;
+    public double primeiroTrimestre;
+    public double segundoTrimestre;
+    public double terceiroTrimestre;
+
+    public String checkResults(){
+        double finalGrade = primeiroTrimestre + segundoTrimestre + terceiroTrimestre;
+        String result;
+        if(finalGrade >= 60){
+           result = finalGrade + "\nPASS";
+        }
+        else{
+            result = "FAILED\nMISSING " + (60 - finalGrade) + " POINTS";
+        }
+        return result;
+    }
+
+    @Override
+    public String toString(){
+      return MessageFormat.format("FINAL GRADE = {0}", checkResults());
+    }
 }
