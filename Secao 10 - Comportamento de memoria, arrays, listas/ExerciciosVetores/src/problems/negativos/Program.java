fb850897b0ca6d525bc2ceaa18a7c99ffc531be2
Pablo Ribeiro <pablorsantana@hotmail.com>
2022-12-28 22:06:54 -0300

    adicionando descricoes dos problemas resolvidos

--- Secao 10 - Comportamento de memoria, arrays, listas/ExerciciosVetores/src/problems/negativos/Program.java
+++ Secao 10 - Comportamento de memoria, arrays, listas/ExerciciosVetores/src/problems/negativos/Program.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        /*Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros
+        e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos.*/
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Quantos numeros voce vai digitar? ");
--- Secao 10 - Comportamento de memoria, arrays, listas/ExerciciosVetores/src/problems/soma_vetor/Program.java
+++ Secao 10 - Comportamento de memoria, arrays, listas/ExerciciosVetores/src/problems/soma_vetor/Program.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        /*Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
+        - Imprimir todos os elementos do vetor
+        - Mostrar na tela a soma e a média dos elementos do vetor*/
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Quantos numeros voce vai digitar? ");
