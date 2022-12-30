1b11b6f0782e087d41899804118b0dc2121f4d9d
Pablo Ribeiro <pablorsantana@hotmail.com>
2022-12-30 01:47:12 -0300

    adicionando projeto 'Matrizes'

---
+++ Secao 10 - Comportamento de memoria, arrays, listas/Matrizes/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Main diagonal");
+        int count = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if(mat[i][j] < 0){
+                    count++;
+                }
+            }
+                System.out.print(mat[i][i] + " ");
+        }
+        System.out.println("\nNegative numbers: " + count);
+    }
+}
