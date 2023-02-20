fccccfdee5143db277584c4ff3574785d5a543d8
Pablo Ribeiro <pablorsantana@hotmail.com>
2023-02-20 20:04:57 -0300

    introducao ao polimorfismo

--- Secao 14 - Heranca e polimorfismo/Heranca/src/Main.java
+++ Secao 14 - Heranca e polimorfismo/Heranca/src/Main.java
@@ -5,6 +5,16 @@ import entities.SavingsAccount;
 public class Main {
     public static void main(String[] args) {
 
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+
+        /*
         Account acc1 = new Account(1001, "Alex", 1000.0);
         acc1.withdraw(200.0);
         System.out.println(acc1.getBalance());
@@ -16,32 +26,7 @@ public class Main {
         Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
         acc3.withdraw(200.0);
         System.out.println(acc3.getBalance());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        */
 
 
 
