f683aded54d1e0b89fa086657cc7624e8ccd495b
Pablo Ribeiro <pablorsantana@hotmail.com>
2023-02-20 17:54:20 -0300

    implementando conceitos de Sobreposicao, 'super' e anotacao @Override

--- Secao 14 - Heranca e polimorfismo/Heranca/src/Main.java
+++ Secao 14 - Heranca e polimorfismo/Heranca/src/Main.java
@@ -4,7 +4,48 @@ import entities.SavingsAccount;
 
 public class Main {
     public static void main(String[] args) {
-        Account acc = new Account(1001, "Alex", 0.0);
+
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* Account acc = new Account(1001, "Alex", 0.0);
         BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
         // UPCASTING
@@ -31,6 +72,6 @@ public class Main {
             SavingsAccount acc5 = (SavingsAccount) acc3;
             acc5.updateBalance();
             System.out.println("Update!");
-        }
+        }*/
     }
 }
